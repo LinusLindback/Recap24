b2cc313ef6336281ce4596dd567fc0c0814d312c
LinusLindback <llinuslindback@gmail.com>
2024-07-24 18:32:55 +0200

    Initial Commit

---
+++ Recap24/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Recap24
+//
+//  Created by Linus Lindbäck on 2024-07-24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
