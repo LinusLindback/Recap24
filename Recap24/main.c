81af1b0e3649265d3756e1b99104bad43ed02085
LinusLindback <llinuslindback@gmail.com>
2024-08-28 12:35:58 +0200

    Första

--- Recap24/main.c
+++ Recap24/main.c
@@ -1,14 +1,12 @@
-//
-//  main.c
-//  Recap24
-//
-//  Created by Linus Lindbäck on 2024-07-24.
-//
-
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main(){
+    int array[] = {0, 1, 2, 3, 4};
+    
+    int *p = array;
+    
+    for (int i = 0; i <5;i++){
+        printf("Element %d: Värde = %d, Adress = %p\n", i, *(p + i), (p + 4));
+    }
     return 0;
 }
